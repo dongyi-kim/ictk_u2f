c47f02c0b97571a43411bf6f648466531aceba70
waps12b <waps12b@naver.com>
2015-02-23 17:30:47 +0900

    test 1

--- server.js
+++ server.js
@@ -69,21 +69,22 @@ app.post('/u2f', function(req,res)
     var registrationData = new Buffer("0504b174bc49c7ca254b70d2e5c207cee9cf174820ebd77ea3c65508c26da51b657c1cc6b952f8621697936482da0a6d3d3826a59095daf6cd7c03e2e60385d2f6d9402a552dfdb7477ed65fd84133f86196010b2215b57da75d315b7b9e8fe2e3925a6019551bab61d16591659cbaf00b4950f7abfe6660e2e006f76868b772d70c253082013c3081e4a003020102020a47901280001155957352300a06082a8648ce3d0403023017311530130603550403130c476e756262792050696c6f74301e170d3132303831343138323933325a170d3133303831343138323933325a3031312f302d0603550403132650696c6f74476e756262792d302e342e312d34373930313238303030313135353935373335323059301306072a8648ce3d020106082a8648ce3d030107034200048d617e65c9508e64bcc5673ac82a6799da3c1446682c258c463fffdf58dfd2fa3e6c378b53d795c4a4dffb4199edd7862f23abaf0203b4b8911ba0569994e101300a06082a8648ce3d0403020347003044022060cdb6061e9c22262d1aac1d96d8c70829b2366531dda268832cb836bcd30dfa0220631b1459f09e6330055722c8d89b7f48883b9089b88d60d1d9795902b30410df304502201471899bcc3987e62e8202c9b39c33c19033f7340352dba80fcab017db9230e402210082677d673d891933ade6f617e5dbde2e247e70423fd5ad7804a6d3d3961ef871", "hex");
 
 
-    var request = u2flib.request(appId,function()
-    {
-        request.challenge = "vqrS6WXDe1JUs5_c3i4-LkKIHRr-3XVb3azuA5TifHo"; // We have a fixed challenge.
-        console.log(' - request ended :' + request);
-
-        var u2f_res = u2flib.checkRegistration(request, result);
-        console.log(u2f_res)
-        if(u2f_res.successful)
-        {
-            console.log('success');
-        }else
-        {
-            console.log('fail');
-        }
-    });
+    //var request = u2flib.request(appId,function()
+    //{
+    //    request.challenge = "vqrS6WXDe1JUs5_c3i4-LkKIHRr-3XVb3azuA5TifHo"; // We have a fixed challenge.
+    //    console.log(' - request ended :' + request);
+    //
+    //    var u2f_res = u2flib.checkRegistration(request, result);
+    //
+    //    console.log(u2f_res)
+    //    if(u2f_res.successful)
+    //    {
+    //        console.log('success');
+    //    }else
+    //    {
+    //        console.log('fail');
+    //    }
+    //});
     console.log(' - request send.');
 
 
