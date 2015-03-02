90102d4590a577894d14fbd75e8e3ec6454ccde1
waps12b <waps12b@naver.com>
2015-03-02 22:29:25 +0900

    config

---
+++ config.js
@@ -0,0 +1,7 @@
+const SERVER_URL = 'www.kd2.kr';
+const APP_ID = 'https://www.kd2.kr';
+
+module.exports = {
+    SERVER_URL: SERVER_URL,
+    APP_ID : APP_ID
+};
--- server.js
+++ server.js
@@ -9,9 +9,10 @@ var https = require('https');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var session = require('session');
+var config = require('./config.js');
 
-var httpsServer = https.createServer({key:fs.readFileSync('key.pem'), cert: fs.readFileSync('cert.pem')},app).listen(443);
-var httpServer = http.createServer(app).listen(80);
+var httpsServer = https.createServer({key:fs.readFileSync('key.pem'), cert: fs.readFileSync('cert.pem')},app);
+//var httpServer = http.createServer(app).liste;
 
 app.set('port',443);
 app.set('view engine', 'ejs');
@@ -33,24 +34,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride());
 
 
-//app.all("*", function(req, res, next)
-//{   //when user request
-//    console.log(' ! user request');
-//    next();
-//
-//    if(res.ended == true)
-//        return;
-//
-//    console.log(' - user access the 404 page ');
-//    res.render('404.ejs', {} );
-//    res.end();
-//    console.log(' request end');
-//});
-
 
 function u2f_send_request()
 {
-    var appId = "https://www.kd2.kr";
+    var appId = config.APP_ID;
     var certificate = new Buffer("3082013c3081e4a003020102020a47901280001155957352300a06082a8648ce3d0403023017311530130603550403130c476e756262792050696c6f74301e170d3132303831343138323933325a170d3133303831343138323933325a3031312f302d0603550403132650696c6f74476e756262792d302e342e312d34373930313238303030313135353935373335323059301306072a8648ce3d020106082a8648ce3d030107034200048d617e65c9508e64bcc5673ac82a6799da3c1446682c258c463fffdf58dfd2fa3e6c378b53d795c4a4dffb4199edd7862f23abaf0203b4b8911ba0569994e101300a06082a8648ce3d0403020347003044022060cdb6061e9c22262d1aac1d96d8c70829b2366531dda268832cb836bcd30dfa0220631b1459f09e6330055722c8d89b7f48883b9089b88d60d1d9795902b30410df", 'hex');
     var clientData = '{"typ":"navigator.id.finishEnrollment","challenge":"vqrS6WXDe1JUs5_c3i4-LkKIHRr-3XVb3azuA5TifHo","cid_pubkey":{"kty":"EC","crv":"P-256","x":"HzQwlfXX7Q4S5MtCCnZUNBw3RMzPO9tOyWjBqRl4tJ8","y":"XVguGFLIZx1fXg3wNqfdbn75hi4-_7-BxhMljw42Ht4"},"origin":"http://example.com"}';
     var publicKey = new Buffer("04b174bc49c7ca254b70d2e5c207cee9cf174820ebd77ea3c65508c26da51b657c1cc6b952f8621697936482da0a6d3d3826a59095daf6cd7c03e2e60385d2f6d9", "hex");
@@ -82,18 +69,25 @@ function u2f_send_request()
     console.log(' - request send.');
 }
 
+app.get('*', function(req,res,next)
+{
+    if(!req.secure)
+    {
+        //redirect http to https
+        return res.redirect('https://' + config.SERVER_URL + req.url);
+    }
+    next();
+});
+
 app.get('/', function(req,res)
 {
     console.log(' - user access the index page ');
     res.render('./index.ejs', {});
 });
+
+
 // yubico u2f id-page
 app.get('/u2f', function(req, res) {
-    if(!req.secure)
-    {
-        //redirect http to https
-        return res.redirect('https://www.' + req.host + req.url);
-    }
     console.log(' - user access the u2f page');
     res.render('./u2f_index.ejs', { user_email : '' , req_Sign_up : false} );
     //res.end();
@@ -111,7 +105,6 @@ app.post('/u2f', function(req,res)
 app.post('/login',function(req,res)
 {
 
-
     res.end('<script> history.go(-1); </script>');
 });
 app.post('/validate',function(req,res)
@@ -122,8 +115,6 @@ app.post('/validate',function(req,res)
 
 /* Main Tab */
 var arrTabMain = ['Home', 'Register', 'Contact', 'About'];
-
-
 
 app.get('/main', function(req,res)
 {
@@ -147,32 +138,5 @@ app.post('/main', function(req,res){
 });
 
 
+app.listen();
 console.log('Server open\n');
-
-
-
-
-
-
-
-
-//http.createServer(function (req, res) {
-//
-//    // Build the answer
-//    var answer = "";
-//    answer += "Request URL : " + req.url + "\n";
-//    answer += "Reqeust type :" + req.method + "\n";
-//    answer += "Request Header : " + JSON.stringify(req.headers) + "\n";
-//
-//
-//    if( req.url == "/"){
-//    	res.writeHead(200, { "Content-Type" : "text/html"});
-//	    res.write('<script src=\"chrome-extension://pfboblefjcgdjicmnffhdgionmgcdmne/u2f-api.js\"></script>');
-//    	res.end('Welcome index');
-//    }else{
-//    	res.writeHead(404, {"Content-Type" : "text/plain"});
-//    	res.end("404 not found.\n");
-//    }
-//
-//}).listen(80, '0.0.0.0');
-//console.log('Server running at http://kd2.kr/');
